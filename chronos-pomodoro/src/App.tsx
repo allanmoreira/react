577d9823d43b99bc8bef6e2d18f03310332eb726
Allan Moreira <allanfelipe.moreira@gmail.com>
2025-12-28 17:14:55 -0300

    css - module

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,7 +1,10 @@
 import './styles/theme.css'
 import './styles/global.css'
+import {Heading} from "./components/Heading.tsx";
 
 export function App() {
     console.log('App is running!');
-    return <h1>Hello, world!</h1>
+    return <>
+        <Heading/>
+    </>
 }
