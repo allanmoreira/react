bb77c27d14cd664d03b09775bb431bc627c1f22c
Allan Moreira <allanfelipe.moreira@gmail.com>
2025-12-28 18:01:12 -0300

    Atualizando css e removendo logs

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -3,7 +3,6 @@ import './styles/global.css'
 import {Heading} from "./components/Heading.tsx";
 
 export function App() {
-    console.log('App is running!');
     return <>
         <Heading attr1={123} attr2={'string'}>Hello, world!</Heading>
     </>
